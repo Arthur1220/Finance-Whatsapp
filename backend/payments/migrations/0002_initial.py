452db8853892f148742526132aadaa9661ac659e
Arthur Marques Azevedo <arthurmarquesazevedo@gmail.com>
2025-09-12 11:24:46 -0300

    feat: Atualiza timestamps das migrações e adiciona gerenciamento de métodos de pagamento

--- backend/payments/migrations/0002_initial.py
+++ backend/payments/migrations/0002_initial.py
@@ -10,14 +10,18 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('incomes', '0001_initial'),
+        ('payments', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='income',
+            model_name='paymentmethod',
             name='user',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='incomes', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='payment_methods', to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AlterUniqueTogether(
+            name='paymentmethod',
+            unique_together={('user', 'name')},
         ),
     ]
